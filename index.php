16d3017ffe7b8de763ce6e44301fb3c6003f374a
jaaanix <janis.saritzoglou@opitz-consulting.de>
2015-05-12 18:05:51 +0200

    fügt card für Film-Cover ein

--- index.php
+++ index.php
@@ -1,36 +1,10 @@
 <?php include "header.html"?>
 <main>
-<!--Import jQuery before materialize.js-->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<nav class="light-blue lighten-1" role="navigation">
-	<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">YouStudy</a>
-	<ul class="right hide-on-med-and-down">
-		<li><a href="#">
-			<i class="large mdi-navigation-apps"></i>
-		</a></li>
-		<li><a href="./parkplatz.html">
-			<i class="large mdi-maps-directions-car"></i>
-		</a></li>
-		<li><a href="./mensa.html">
-			<i class="large mdi-maps-local-restaurant"></i>
-		</a></li>
-		<li><a href="./kino.html">
-			<i class="large mdi-action-theaters"></i>
-		</a></li>
-	</ul>
-	
-	<ul id="nav-mobile" class="side-nav" style="left: -250px;">
-		<li><a href="#"><i class="large mdi-navigation-apps"></i></a></li>
-	</ul>
-	<a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i	></a>
-</div>
-</nav>
-<a href="www.google.de" id="google_btn" class="btn-large waves-effect waves-light orange">Google</a>
-<a href="www.iphone-ticker.de" id="ifun_btn" class="btn-large waves-effect waves-light blue">iPhone-Ticker</a>
-<a href="www.computerbase.de" id="cb_btn" class="btn-large waves-effect waves-light green">ComputerBase</a>
-<a href="www.google.de" id="ico_btn" class="btn-floating btn-large waves-effect waves-light red">
-<i class="large mdi-maps-local-restaurant"></i></a>
-<img src="cat.gif">
-</main>
-<?php include "footer.html"?>
+	<a href="www.google.de" id="google_btn" class="btn-large waves-effect waves-light orange">Google</a>
+	<a href="www.iphone-ticker.de" id="ifun_btn" class="btn-large waves-effect waves-light blue">iPhone-Ticker</a>
+	<a href="www.computerbase.de" id="cb_btn" class="btn-large waves-effect waves-light green">ComputerBase</a>
+	<a href="www.google.de" id="ico_btn" class="btn-floating btn-large waves-effect waves-light red">
+		<i class="large mdi-maps-local-restaurant"></i></a>
+		<img src="cat.gif">
+	</main>
+	<?php include "footer.html"?>
---
+++ kino.php
@@ -0,0 +1,19 @@
+<?php include "header.html"?>
+<main>
+	<div class="col s12 m7">
+		<div class="card small">
+			<div class="card-image waves-effect waves-block waves-light">
+				<img class="activator" src="images/madmax.jpg">
+			</div>
+			<div class="card-content">
+				<span class="card-title activator grey-text text-darken-4">Mad Max<i class="mdi-navigation-more-vert right"></i></span>
+				<p><a href="#">This is a link</a></p>
+			</div>
+			<div class="card-reveal">
+				<span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+				<p>Here is some more information about this product that is only revealed once clicked on.</p>
+			</div>
+		</div>
+	</div>
+</main>
+<?php include "footer.html"?>
