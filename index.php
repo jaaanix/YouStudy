b6b60434eea02cd727302fbf3aa2f4970ffa2d52
jaaanix <janis.saritzoglou@opitz-consulting.de>
2015-08-31 21:05:45 +0200

    fügt häßliche provisorische Ampeln auf die Startseite ein

--- index.php
+++ index.php
@@ -15,8 +15,48 @@
                 <img src="cat.gif">
             </div>
             <div class="box50">
+            <style type="text/css">.lights {
+    background: #444;
+    float: left;
+    font-size: 60px;
+    height: 60px;
+    margin: 10px;
+    padding: 4px;
+    width: 20px;
+}
+.lights span {
+    color: #000;
+    display: block;
+    line-height: 20px;
+    text-align: center;
+    width: 20px;
+}
+.lights.green .bottom {
+    color: lime;
+}
+.lights.yellow .middle {
+    color: yellow;
+}
+.lights.red .top {
+    color: red;
+}</style>
                 <span class="caption">Parkplatzsituation</span>
-
+                    Frei Parkplätze: 23
+                    <div class="lights red">
+                      <span class="top">&bull;</span>
+                      <span class="middle">&bull;</span>
+                      <span class="bottom">&bull;</span>
+                    </div>
+                    <div class="lights red yellow">
+                      <span class="top">&bull;</span>
+                      <span class="middle">&bull;</span>
+                      <span class="bottom">&bull;</span>
+                    </div>
+                    <div class="lights green">
+                      <span class="top">&bull;</span>
+                      <span class="middle">&bull;</span>
+                      <span class="bottom">&bull;</span>
+                    </div>
             </div>
         </div>
 
