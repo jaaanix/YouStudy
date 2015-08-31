c8391b7514d5a744936fe3b33a6df0d847b3968f
jaaanix <janis.saritzoglou@opitz-consulting.de>
2015-08-31 20:46:54 +0200

    fügt ersten Mensa Entwurf hinzu

--- mensa.php
+++ mensa.php
@@ -1,7 +1,66 @@
 <?php include 'header.tpl'; ?>
 
+    <main>
 
+        <div class="row">
+            <div class="box50">
+                <span class="caption">Gerichte heute</span>
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;border-top-width:1px;border-bottom-width:1px;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;border-top-width:1px;border-bottom-width:1px;}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;}}</style>
+<table class="tg">
+  <tr>
+    <th class="tg-031e">Gericht</th>
+    <th class="tg-031e">Preis</th>
+    <th class="tg-031e">Bewertung</th>
+  </tr>
+  <tr>
+    <td class="tg-031e">Curry</td>
+    <td class="tg-031e"><ul><li>1,95€</li><li>2,25</li><li>4,10€</li></ul></td>
+    <td class="tg-031e"><i class="fa fa-star"><i class="fa fa-star"></i><i class="fa fa-star"></i></i></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Würstl</td>
+    <td class="tg-031e"><ul><li>1,95€</li><li>2,25</li><li>4,10€</li></ul></td>
+    <td class="tg-031e"><i class="fa fa-star"></i></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Nudeln</td>
+    <td class="tg-031e"><ul><li>1,95€</li><li>2,25</li><li>4,10€</li></ul></td>
+    <td class="tg-031e"><i class="fa fa-star"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></i></td>
+  </tr>
+</table>
+                <img src="cat.gif">
+            </div>
+            <div class="box50">
+                <span class="caption">Gerichte morgen</span>
+<table class="tg">
+  <tr>
+    <th class="tg-031e">Gericht</th>
+    <th class="tg-031e">Preis</th>
+    <th class="tg-031e">Bewertung</th>
+  </tr>
+  <tr>
+    <td class="tg-031e">Curry</td>
+    <td class="tg-031e"><ul><li>1,95€</li><li>2,25</li><li>4,10€</li></ul></td>
+    <td class="tg-031e"><i class="fa fa-star"><i class="fa fa-star"></i><i class="fa fa-star"></i></i></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Würstl</td>
+    <td class="tg-031e"><ul><li>1,95€</li><li>2,25</li><li>4,10€</li></ul></td>
+    <td class="tg-031e"><i class="fa fa-star"></i></td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Nudeln</td>
+    <td class="tg-031e"><ul><li>1,95€</li><li>2,25</li><li>4,10€</li></ul></td>
+    <td class="tg-031e"><i class="fa fa-star"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></i></td>
+  </tr>
+</table>
+            </div>
+        </div>
 
-
+    </main>
 
 <?php include 'footer.tpl'; ?>
