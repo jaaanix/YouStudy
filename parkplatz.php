5e50632ae92530ff859e29731b1bdecfb8254480
Marvin Muuß <marvinmuuss@gmx.de>
2015-08-31 21:00:43 +0200

    Erste Schritte Parkplatz

--- parkplatz.php
+++ parkplatz.php
@@ -1,7 +1,41 @@
 <?php include 'header.tpl'; ?>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<link href="css/grids.css" rel="stylesheet">
+	<link href="css/lights.css" rel="stylesheet">
+</head>
+<body>
 
+	<div class="section group">
+		<div class="col span_1_of_2">
+			<div class = "center deco-box">
+				<br>
+				<div class="section group">
+				<div class="col span_1_of_3">
+						<div id="light">
+							<span class="active" id="red"></span>
+							<span id="orange"></span>
+							<span id="green"></span>
+						</div>
+					</div>
+					<div class="col span_2_of_3">
+						<div class = "park-text">
+							Parkplätze frei: 47 <br><br><br>
+							Parkplätze belegt: 213
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col span_1_of_2">
+			<div>
+			<img src="imgages/parkplatz.jpg" >
+			</div>
+		</div>
+	</div>
 
-
-
+</body>
+</html>
 
 <?php include 'footer.tpl'; ?>
