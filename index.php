186ebaac2bfb0d7741d540e8f764345a9f7c0ec6
stelzenlaeufer <gebauer.wiehl@web.de>
2015-09-01 02:36:25 +0200

    no message

--- index.php
+++ index.php
@@ -6,14 +6,11 @@
             <div class="row">
                 <div class="box50">
                     <span class="caption">Aktuelle Mitteilungen</span>
-                    <ul>
-                        <li> 31.08.2015 <b>Die Seite ist Online!</b></li>
-                        <li> 30.08.2015 <b>Wieder verwackelt:</b> Lebensmittelfotograf verzweifelt an Pudding</li>
-                        <li> 28.08.2015 <b>Glutenfreies Brot:</b> Feuerwehr löscht Brand in Bio-Bäckerei vollständig</b></li>
-
-
+                    <ul id="news">
+                        <li class="news"> 31.08.2015 <b>Die Seite ist Online!</b></li>
+                        <li class="news"> 30.08.2015 <b>Wieder verwackelt:</b> Lebensmittelfotograf verzweifelt an Pudding</li>
+                        <li class="news"> 28.08.2015 <b>Glutenfreies Brot:</b> Feuerwehr löscht Brand in Bio-Bäckerei vollständig</b></li>
                     </ul>
-                    <img src="cat.gif">
                 </div>
                 <div class="box50">
                     <span class="caption">Parkplatzsituation</span>
